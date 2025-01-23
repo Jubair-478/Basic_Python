237b00d007fcc47b0c14182f4c75b8692bebfff4
Jubair-478 <jujupy487@gmail.com>
2025-01-23 16:12:14 +0600

    23_01.py

--- 23_01.py
+++ 23_01.py
@@ -1,15 +1,47 @@
-def calc_sum(a,b):
-    sum = a+b
-    print(sum)
-    return sum
+# def calc_sum(a,b):
+#     sum = a+b
+#     print(sum)
+#     return sum
 
 
 
 
-calc_sum(5,10)
+# calc_sum(5,10)
 
-calc_sum(100,1)
+# calc_sum(100,1)
 
-calc_sum(1,9)
+# calc_sum(1,9)
+
+# def calc_def(a,b):
+#     return a-b
+
+# cal=calc_def(9,99)
+# print(cal)
+
+# def print_hello():
+#     print("Hi Babes")
+
+# print_hello()
+# print_hello()
+# print_hello()
+
+# def print_hello():
+#      print("Hi Babes")
+
+# output=print_hello()
+# print(output)
+
+# average of 3 numbers
+
+def numbers(a,b,c):
+    sum=a+b+c
+    average=sum/3
+    print(average)
+    return average
+
+numbers(1,2,3)
+numbers(999,888,777)
+numbers(23,22,21)
+
 
 
