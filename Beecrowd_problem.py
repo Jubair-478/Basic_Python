4d8dc14e1679b50a5f124150392588a102c7690d
Jubair-478 <jujupy487@gmail.com>
2025-01-25 21:48:21 +0600

    beecrowd

---
+++ Beecrowd_problem.py
@@ -0,0 +1,93 @@
+# Problem - 1
+# Welcome to beecrowd!
+
+# Your first program in any programming language is usually "Hello World!". In this first problem all you have to do is print this message on the screen.
+
+# Input
+# This problem has no input.
+
+# Output
+# You must print the message Hello World! and then the endline as shown below.
+
+# Input Sample	Output Sample
+# Hello World!/////
+
+# print("Hello World!")
+
+# Problem - 2
+# Read 2 variables, named A and B and make the sum of these two variables, assigning its result to the variable X. Print X as shown below. Print endline after the result otherwise you will get “Presentation Error”.
+
+# Input
+# The input file will contain 2 integer numbers.
+
+# Output
+# Print the letter X (uppercase) with a blank space before and after the equal signal followed by the value of X, according to the following example.
+
+# Obs.: don't forget the endline after all.
+
+# Samples Input	Samples Output
+# 10
+# 9
+
+# X = 19
+
+# -10
+# 4
+
+# X = -6
+
+# 15
+# -7
+
+# X =
+
+
+
+# a = int(input())
+# b = int(input())
+
+# x = a + b
+
+# print('X =', x)
+
+# The formula to calculate the area of a circumference is defined as A = π . R2. Considering to this problem that π = 3.14159:
+
+# Calculate the area using the formula given in the problem description.
+
+# Input
+# The input contains a value of floating point (double precision), that is the variable R.
+
+# Output
+# Present the message "A=" followed by the value of the variable, as in the example bellow, with four places after the decimal point. Use all double precision variables. Like all the problems, don't forget to print the end of line after the result, otherwise you will receive "Presentation Error".
+
+# Input Samples	Output Samples
+# 2.00
+
+# A=12.5664
+
+# 100.64
+
+# A=31819.3103
+
+# 150.00
+
+# A=70685.7750
+
+
+# problem-3
+
+# R = float(input())
+
+# A=3.14159*(R**2)
+
+# print("A=%.4f"%A)
+
+# problem-1003
+
+
+SO=int(input())
+MA=int(input())
+print("SOMA =",SO+MA)
+
+# problem-1004
+
