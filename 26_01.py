10dd581bc13c0ab674b98a63c2c2ffe702835b31
Jubair-478 <jujupy487@gmail.com>
2025-01-26 22:11:35 +0600

    26_01.py

---
+++ 26_01.py
@@ -0,0 +1,14 @@
+#Write a recursive function to calculate the sum of first n natural numbers.
+
+def func(n):
+    if (n==0):
+        return 0
+    return n + func(n - 1)
+
+
+    
+
+print(func(5))
+
+
+
