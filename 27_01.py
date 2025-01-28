782d7b3afdf4099ea5a4e226891c6dea964b2f9b
Jubair-478 <jujupy487@gmail.com>
2025-01-28 23:52:37 +0600

    Update 28_01_practice.py

---
+++ 27_01.py
@@ -0,0 +1,18 @@
+#  >>>>>>> string formatting<<<<<<<
+
+# letter ="HI {},Are you from {}"
+# name="Harry"
+# country="GU"
+# print(letter.format(name,country))
+
+# letter ="HI {1},Are you from {0}"
+# name="Harry"
+# country="GU"
+# print(letter.format(country,name))
+
+# price =4.9849848976497
+# print(f"price is {price:.2f}")
+
+print(f"{2*3}")
+
+
