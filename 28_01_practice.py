5eaae6a0a8cd764754b70cef3ea078786a541851
Jubair-478 <jujupy487@gmail.com>
2025-01-28 23:51:59 +0600

    Update 28_01_practice.py

---
+++ 28_01_practice.py
@@ -0,0 +1,60 @@
+"""
+Input Format
+
+You do not need to read any input in this challenge.
+
+Output Format
+
+Print Hello, World! to stdout.
+
+Sample Output 0
+
+"""
+
+# print("Hello, World!")
+
+"""
+Task
+Given an integer, , perform the following conditional actions:
+
+If  is odd, print Weird
+If  is even and in the inclusive range of  to , print Not Weird
+If  is even and in the inclusive range of  to , print Weird
+If  is even and greater than , print Not Weird
+Input Format
+
+A single line containing a positive integer, .
+
+Constraints
+
+Output Format
+
+Print Weird if the number is weird. Otherwise, print Not Weird.
+
+"""
+# n = int(input())
+# if n%2 == 0:
+#     print("Not Weird")
+# elif 2<=n<=5:
+#    print("Not Weird")
+# elif n>20:
+#    print("Not Weird")
+# elif 6<=n<=20:
+#     print("Weird")
+# else:
+#     print("Weird")
+
+
+# n = int(input())
+# if n%2 == 0:
+#     print("Not Weird")
+# elif 2<=n<=5:
+#    print("Not Weird")
+# elif n>20:
+#    print("Not Weird")
+# elif 6<=n<=20:
+#     print("Weird")
+# else:
+#     print("Weird")
+
+
