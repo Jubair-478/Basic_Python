6563c2ba99369160164db6401f4b36c87ba3ee41
Jubair-478 <jujupy487@gmail.com>
2025-01-29 22:14:31 +0600

    Add 29_01.py

---
+++ 29_01.py
@@ -0,0 +1,82 @@
+# n=int(input())
+
+# if n%2 ==0 and 2<=n<=5:
+#     print("Not Weird")
+# elif n%2 ==0 and n>20:
+#     print("Not Weird")
+# elif n%2 ==0 and 6<=n<=20:
+#     print("Weird")
+# else:
+#     print("Weird")
+
+
+# a =int(input())
+# b=int(input())
+
+# print(a+b)
+# print(a-b)
+# print(a*b)
+
+
+
+# a=int(input())
+# b=int(input())
+
+# print(a//b)
+# print(a/b)
+
+
+
+# n=int(input())
+# for n in range(0,n+1):
+#     print(n**2)
+    
+# n=int(input())
+# def calender(n):
+#     if n % 4 == 0:
+#         if n % 100 == 0:
+#             if n % 400 == 0:
+#                 return True
+#             else:
+#                 return False
+#         else:
+#             return True  
+#     else:
+#         return False        
+
+# print(calender(n))
+
+# n = int(input())
+# def calendar(n):
+#     if n % 4 == 0:
+#         if n % 100 == 0:
+#             if n % 400 == 0:
+#                 return True
+#             else:
+#                 return False
+#         else:
+#             return True
+#     else:
+#         return False
+
+# print(calendar(n))
+
+
+# n = int(input())
+
+# for n in range(1,n+1):
+#     print(n,end="")
+
+# dont give me answer give me a breif explaination in each topic so easily with example
+# ls  
+# git add 29_01.py
+# git commit -m "Add 29_01.py"
+# git remote add origin https://github.com/Jubair-478/repository.git 
+# git push origin main  
+
+# ls  
+# git add 29_01.py
+# git commit -m "29_01.py"
+# git remote add origin https://github.com/Jubair-478/repository.git 
+# git push origin main 
+
