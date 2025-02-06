929ff1dbc3265f676306d69bec3641737e9720e0
Jubair-478 <jujupy487@gmail.com>
2025-02-06 22:35:25 +0600

    pattern.py

---
+++ pattern.py
@@ -0,0 +1,72 @@
+# patter 1
+# * * * * * * 
+# * * * * * * 
+# * * * * * * 
+# * * * * * * 
+# * * * * * * 
+# * * * * * * 
+
+
+# for i in range (0,6):
+#     for j in range (0,6):
+#         print("*",end =" ")
+#     print()
+    
+
+# pattern 2
+# * 
+# * * 
+# * * * 
+# * * * * 
+# * * * * *
+# * * * * * *
+
+# for i in range(0,6):
+#     for j in range(i+1):
+#         print("*",end=" ")
+#     print()
+
+
+
+# pattern 3
+# * 
+# * *
+# * * *
+# * * * *
+# * * * * *
+#
+# 
+#  for i in range(0,6):
+#     for j in range(i):
+#         print("*",end=" ")
+#     print()
+
+
+# pattern 4
+# 1 
+# 1 2 
+# 1 2 3 
+# 1 2 3 4 
+# 1 2 3 4 5 
+
+
+# for i in range(0,6):
+#     for j in range(1,i+1):
+#         print(j,end=" ")
+#     print()
+
+
+
+
+# pattern 5
+# 5 4 3 2 1 
+# 4 3 2 1 
+# 3 2 1
+# 2 1
+# 1
+# for i in range(5,0,-1):
+#     for j in range(i,0,-1):
+#         print(j,end=" ")
+#     print()
+
+
